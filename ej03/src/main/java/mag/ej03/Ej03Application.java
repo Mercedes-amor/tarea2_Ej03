806159391ed3e60abd74cc3ffe3359389fe3cb4f
MercedesAmorGallart <127596167+MercedesAmorGallart@users.noreply.github.com>
2024-10-18 13:26:40 +0200

    init

---
+++ ej03/src/main/java/mag/ej03/Ej03Application.java
@@ -0,0 +1,14 @@
+package mag.ej03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej03Application.class, args);
+		System.out.println("Hola mundo ejercicio 2.3 - David Bowie");
+	}
+
+}
---
+++ ej03/src/test/java/mag/ej03/Ej03ApplicationTests.java
@@ -0,0 +1,13 @@
+package mag.ej03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
